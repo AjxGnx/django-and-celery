96c119af2efcb4de6eb5e9bb5e0f260b0ccb0562
alirio <alirio1925@gmail.com>
2019-07-17 20:14:54 -0400

    [ADD] task send email

--- proj/demoapp/tasks.py
+++ proj/demoapp/tasks.py
@@ -1,18 +1,22 @@
-# Create your tasks here
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
+from proj.celery import app
+from django.core.mail import send_mail
+from proj import settings
 
 
-@shared_task
-def add(x, y):
-    return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
-
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
+@app.task(bind=True, max_retries=10)
+def send_emails_task(self, array_emails, message):
+    name = 'CELERY AND DJANGO'
+    subject = 'TEST TASK CELERY'
+    email_from = settings.EMAIL_HOST_USER
+    email_to = array_emails
+    message_email = "{}----{}".format(message, name)
+    try:
+        send_mail(subject,
+                  message_email,
+                  email_from,
+                  email_to,
+                  fail_silently=False
+                  )
+    except Exception as exc:
+        raise self.retry(exc=exc, countdown=15)
