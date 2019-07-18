86c329766f98da3c1a5a20e25f2b3a230f211270
alirio <alirio1925@gmail.com>
2019-07-17 20:15:27 -0400

    [ADD] def send email

--- proj/demoapp/views.py
+++ proj/demoapp/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .tasks import send_emails_task
 
-# Create your views here.
+
+# Vista que llama a la tarea.
+def send_emails(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    elif request.method == 'POST':
+        send_emails_task.delay([request.POST['array_emails']], request.POST['message'])
+        return HttpResponse('Emails sended using Celery')
